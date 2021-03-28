97861d80b2decdb0d4d02b09488e7c4ff373b0b2
artadmire <artadmire@163.com>
2021-03-28 20:04:41 +0800

    feat: 去掉虚线

--- src/components/myBottom/index.js
+++ src/components/myBottom/index.js
@@ -6,11 +6,11 @@ function myBottom(props) {
   return (
     <div className={`myBottom ${className}`}>
       <div className="icon">
-        <img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/twitter%402x.png' />
-        <img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/discord%402x.png'/>
-        <img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/medium%402x.png'/>
-        <img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/telegram%402x.png'/>
-        <img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/email%402x.png'/>
+        <p><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/twitter%402x.png' /></p>
+        <p><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/discord%402x.png'/></p>
+        <p><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/medium%402x.png'/></p>
+        <p><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/telegram%402x.png'/></p>
+        <p><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/email%402x.png'/></p>
       </div>
       <p className="contact">contact@ffcap.com</p>
       <p className="copy">© 2021 FFCAP</p>
