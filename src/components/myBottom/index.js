cff027b7797716d87dad1601f2c4845047bbf16f
artadmire <artadmire@163.com>
2021-04-09 12:27:11 +0800

    feat: 修改地址

--- src/components/myBottom/index.js
+++ src/components/myBottom/index.js
@@ -6,13 +6,13 @@ function myBottom(props) {
   return (
     <div className={`myBottom ${className}`}>
       <div className="icon">
-        <a href="javascript:;"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/twitter%402x.png' /></a>
-        <a href="javascript:;"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/discord%402x.png'/></a>
-        <a href="javascript:;"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/medium%402x.png'/></a>
-        <a href="javascript:;"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/telegram%402x.png'/></a>
-        <a href="javascript:;"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/email%402x.png'/></a>
+        <a target='_blank' href="https://twitter.com/FFCAP1" rel="noreferrer"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/twitter%402x.png' /></a>
+        <a target='_blank' href="https://discord.gg/Cas3g8bK" rel="noreferrer"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/discord%402x.png'/></a>
+        <a target='_blank' href="https://medium.com/@ffcap.pro" rel="noreferrer"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/medium%402x.png'/></a>
+        <a target='_blank' href="https://t.me/FFCAP_pro" rel="noreferrer"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/telegram%402x.png'/></a>
+        <a  href="javascript:;" rel="noreferrer"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/email%402x.png'/></a>
       </div>
-      <p className="contact">contact@ffcap.com</p>
+      <p className="contact">business@ffcap.pro</p>
       <p className="copy">© 2021 FFCAP</p>
     </div>
   );
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -7,17 +7,17 @@ function Contact() {
         <img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/%E4%BD%8D%E5%9B%BE%402x.png'/>
         <p >Contact By App</p>
         <div className="icons">
-          <a href="javascript:;"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/twitter%402x.png'/></a>
-          <a href="javascript:;"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/discord%402x.png'/></a>
-          <a href="javascript:;"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/medium%402x.png'/></a>
-          <a href="javascript:;"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/telegram%402x.png'/></a>
+          <a target='_blank' href="https://twitter.com/FFCAP1" rel="noreferrer"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/twitter%402x.png'/></a>
+          <a target='_blank' href="https://discord.gg/Cas3g8bK" rel="noreferrer"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/discord%402x.png'/></a>
+          <a target='_blank' href="https://medium.com/@ffcap.pro" rel="noreferrer"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/medium%402x.png'/></a>
+          <a target='_blank' href="https://t.me/FFCAP_pro" rel="noreferrer"><img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/telegram%402x.png'/></a>
         </div>
       </div>
-      <div className="card">
+      <a  href="javascript:;" className="card" rel="noreferrer">
         <img src='https://ffcap.oss-cn-hangzhou.aliyuncs.com/img/%E4%BD%8D%E5%9B%BE%402x%202.png'/>
         <p>Contact By Email</p>
-        <span>contact@ffcap.com</span>
-      </div>
+        <span>business@ffcap.pro</span>
+      </a>
     </div>
   );
 }
